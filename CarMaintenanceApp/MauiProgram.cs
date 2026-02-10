c9b41042e55ad84df308624b3f10eae510e8238a
Qaiser Mehmood <qmmughal@gmail.com>
2026-02-11 01:40:45 +0500

    Create .NET MAUI oil change tracker app for iOS and Android

---
+++ CarMaintenanceApp/MauiProgram.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Logging;
+
+namespace CarMaintenanceApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
